39119d533b5dd51e26967afee9a3d39d9fdd0e30
liber09 <linda.bergsangel@hotmail.com>
2023-01-25 04:09:25 +0100

    Opening hours and contact

--- js/index.js
+++ js/index.js
@@ -6,8 +6,14 @@ const app = express();
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
+//Index page
 app.get("/", (req,res) => {
     res.render('index')
+});
+
+//Opening hours and contact
+app.get("/openingHours", (req,res) => {
+    res.render('openingHoursAndContact')
 });
 
 app.use('/static',express.static('./static'));
