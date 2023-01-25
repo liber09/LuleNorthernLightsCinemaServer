799bef8e96cc4bab896af8fe6a1f750d64be501e
liber09 <linda.bergsangel@hotmail.com>
2023-01-25 04:01:13 +0100

    Use ejs as template engine

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
-const app = require('./app.js');
+import express from 'express';
+import ejs from 'ejs';
+
+
+const app = express();
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.get("/", (req,res) => {
+    res.render('index')
+});
+
+app.use('/static',express.static('./static'));
 
 app.listen(5080);
