8e0c9c57ebf724868f14a5415f6fa9973a5f11b3
liber09 <linda.bergsangel@hotmail.com>
2023-01-26 05:43:51 +0100

    implementerat tester

---
+++ index.js
@@ -0,0 +1,3 @@
+import app from './js/app.js';
+
+app.listen(5080);
---
+++ tests/pages.spec.js
@@ -0,0 +1,24 @@
+import request from 'supertest';
+import app from '../js/app.js';
+
+//Define test name
+test('home page shows list of movies', async () => {
+  const response = await request(app) //send request
+    .get('/')  //To what path
+    .expect('Content-Type', 'text/html; charset=utf-8') //Expected return type
+    .expect(200); //the expected response code
+
+  expect(response.text.includes('Shawshank')).toBeTruthy(); //Expext this to be included in response
+  expect(response.text.includes('Encanto')).toBeTruthy(); //Expect this to be in the response
+});
+
+//Define test name
+test('Shawshank page returns Shawshank info', async () => {
+  const response = await request(app) //Send request
+    .get('/movies/3')  //To what path
+    .expect('Content-Type', 'text/html; charset=utf-8')  //Expected return type
+    .expect(200);
+
+  expect(response.text.includes('Shawshank')).toBeTruthy();  //Expected to be in response
+  expect(response.text.includes('dogs')).toBeFalsy();  //Expected not to be in response
+});
