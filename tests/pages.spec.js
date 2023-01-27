5454ba438d5bf554c731a36c0b914ec2582d345b
liber09 <linda.bergsangel@hotmail.com>
2023-01-27 20:59:10 +0100

    Added some more tests

--- tests/pages.spec.js
+++ tests/pages.spec.js
@@ -12,6 +12,7 @@ test('home page shows list of movies', async () => {
   expect(response.text.includes('Encanto')).toBeTruthy(); //Expect this to be in the response
 });
 
+//#region "Movie tests"
 //Define test name
 test('Shawshank page returns Shawshank info', async () => {
   const response = await request(app) //Send request
@@ -19,9 +20,51 @@ test('Shawshank page returns Shawshank info', async () => {
     .expect('Content-Type', 'text/html; charset=utf-8')  //Expected return type
     .expect(200);
 
-  expect(response.text.includes('Shawshank')).toBeTruthy();  //Expected to be in response
+    expect(response.text.includes('The Shawshank Redemption')).toBeTruthy();  //Expected to be in response
+    expect(response.text.includes('dogs')).toBeFalsy();  //Expected not to be in response
+    expect(response.text.includes('Encanto')).toBeFalsy();  //Expected not to be in response
+    expect(response.text.includes('Totoro')).toBeFalsy();  //Expected not to be in response
+});
+
+//Define test name
+test('Encanto page returns Encanto info', async () => {
+  const response = await request(app) //Send request
+    .get('/movies/2')  //To what path
+    .expect('Content-Type', 'text/html; charset=utf-8')  //Expected return type
+    .expect(200);
+
+  expect(response.text.includes('Encanto')).toBeTruthy();  //Expected to be in response
+  expect(response.text.includes('dogs')).toBeFalsy();  //Expected not to be in response
+  expect(response.text.includes('Shawshank')).toBeFalsy();  //Expected not to be in response
+  expect(response.text.includes('Totoro')).toBeFalsy();  //Expected not to be in response
+});
+
+//Define test name
+test('Isle of dogs page returns Isle of dogs info', async () => {
+  const response = await request(app) //Send request
+    .get('/movies/1')  //To what path
+    .expect('Content-Type', 'text/html; charset=utf-8')  //Expected return type
+    .expect(200);
+
+  expect(response.text.includes('Isle of dogs')).toBeTruthy();  //Expected to be in response
+  expect(response.text.includes('Encanto')).toBeFalsy();  //Expected not to be in response
+  expect(response.text.includes('Shawshank')).toBeFalsy();  //Expected not to be in response
+  expect(response.text.includes('Totoro')).toBeFalsy();  //Expected not to be in response
+});
+
+//Define test name
+test('Isle of dogs page returns Isle of dogs info', async () => {
+  const response = await request(app) //Send request
+    .get('/movies/4')  //To what path
+    .expect('Content-Type', 'text/html; charset=utf-8')  //Expected return type
+    .expect(200);
+
+  expect(response.text.includes('Totoro')).toBeTruthy();  //Expected to be in response
+  expect(response.text.includes('Encanto')).toBeFalsy();  //Expected not to be in response
+  expect(response.text.includes('Shawshank')).toBeFalsy();  //Expected not to be in response
   expect(response.text.includes('dogs')).toBeFalsy();  //Expected not to be in response
 });
+//#endregion
 
 //Define test name
 test('movie not found', async () => {
